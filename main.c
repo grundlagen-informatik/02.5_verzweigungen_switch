29a70fdc12d91108042771feaaa71f89e32167f7
Isabel <43370060+isabelge@users.noreply.github.com>
2023-09-16 08:32:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,17 +9,13 @@ void maxDreiZahlen();
 // Funktionsprototyp für Aufgabe 2
 void tageMonat();
 
-// Funktionsprototyp für Aufgabe 3
-void pyramide();
-
 // Am Hauptteil muss nichts geändert werden. Der Code kann in die vier Funktionen geschrieben werden.
 int main() {
     int aufgabe;
 
     printf("Fuer Maximum-Aufgabe mit zwei Zahlen druecke: 1\n"
            "Fuer Maximum-Aufgabe mit drei Zahlen druecke: 2\n"
-           "Fuer Anzahl Tage im Monat druecke: 3\n"
-           "Fuer Pyramide druecke: 4\n");
+           "Fuer Anzahl Tage im Monat druecke: 3\n");
     scanf("%d", &aufgabe);
 
     if (aufgabe == 1) {
@@ -28,8 +24,6 @@ int main() {
         maxDreiZahlen();
     } else if (aufgabe == 3){
         tageMonat();
-    } else if (aufgabe == 4){
-        pyramide();
     } else {
         printf("Ungueltige Eingabe, Programm muss neu gestartet werden.\n");
     }
@@ -46,8 +40,4 @@ void maxDreiZahlen(){
 
 void tageMonat(){
     // Hier muss der Code für die Anzahl Tage im Monat geschrieben werden.
-}
-
-void pyramide(){
-    // Hier muss der Code für die Pyramide geschrieben werden.
 }
